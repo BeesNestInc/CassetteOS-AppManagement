78d3e7e18ec162d31c1e233a666cd19e93f045cb
mayumi <mayumi@beesnest-inc.com>
2025-07-13 06:02:38 +0000

    fix: envファイルがアップデート時に上書きされる不具合の対応

--- build/scripts/setup/service.d/app-management/arch/setup-app-management.sh
+++ build/scripts/setup/service.d/app-management/arch/setup-app-management.sh
@@ -9,10 +9,17 @@ readonly APP_NAME_SHORT="app-management"
 readonly CONF_PATH=/etc/cassetteos
 readonly CONF_FILE=${CONF_PATH}/${APP_NAME_SHORT}.conf
 readonly CONF_FILE_SAMPLE=${CONF_PATH}/${APP_NAME_SHORT}.conf.sample
+readonly ENV_FILE=${CONF_PATH}/env
+readonly ENV_FILE_SAMPLE=${CONF_PATH}/env.sample
 
 if [ ! -f "${CONF_FILE}" ]; then \
     echo "Initializing config file..."
     cp -v "${CONF_FILE_SAMPLE}" "${CONF_FILE}"; \
+fi
+
+if [ ! -f "${ENV_FILE}" ]; then \
+    echo "Initializing env file..."
+    cp -v "${ENV_FILE_SAMPLE}" "${ENV_FILE}"; \
 fi
 
 systemctl daemon-reload
